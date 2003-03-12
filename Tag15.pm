cec089acc3bfdb3ad971518e398dd47b5b6f58a2
Frank Dupont <biker_cpan@hotmail.com>
2003-03-12 20:28:14 +0000

    Import of BIKER/SWIFT-Factory-Tag-Tag15-0.02 from CPAN.
    gitpan-cpan-distribution: SWIFT-Factory-Tag-Tag15
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BIKER/SWIFT-Factory-Tag-Tag15-0.02.tar.gz
    gitpan-cpan-author:       BIKER
    gitpan-cpan-maturity:     released

--- Tag15.pm
+++ Tag15.pm
@@ -15,7 +15,7 @@ BEGIN{
 }
 
 use vars qw($VERSION);
-$VERSION='0.01';
+$VERSION='0.02';
 
 ###########################################################################################################
 sub new {
@@ -268,11 +268,11 @@ Will return the version of this Perl module.
 
 =head1 AUTHOR
 
-Gustav Schaffter, E<lt>schaffter_cpan@hotmail.comE<gt>
+BIKER, E<lt>biker_cpan@hotmail.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2003, Gustav Schaffter. All rights reserved.
+Copyright (c) 2003, BIKER. All rights reserved.
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
